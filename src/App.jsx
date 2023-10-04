3a2629e05aa16f3f4cb9105137da0deaf5fcb434
prathamdupare <prathamdupare@proton.me>
2023-10-04 12:26:21 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
